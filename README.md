2fd7337e96d8a36e57565534eee4c957af5911be
Fred Campo <rickalburo@gmail.com>
2023-01-25 20:16:35 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Udemy Svelte JS
+
+Compilation of output and guide notes when studying the course. Course [link](https://www.udemy.com/course/svelte-tutorial-and-projects-course)
+
+## Topics
+
+- [Budget Calculator](/budget-calc)
---
+++ budget-calc/src/App.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  let test = 'hello'
+</script>
+
+<svelte:head>
+  <link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"
+    integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w=="
+    crossorigin="anonymous"
+    referrerpolicy="no-referrer"
+  />
+  <title>Budget Calculator</title>
+</svelte:head>
+
+<h1><i class="fa-solid fa-user fa-fw" /> {test}</h1>
+<i class="far fa-user fa-fw" /> asdasd
+
+<div>
+  <i class="fas fa-clipboard-check fa-fw" /> test
+</div>
