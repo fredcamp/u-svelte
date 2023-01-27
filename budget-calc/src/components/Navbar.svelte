a69d765568f6d317d2618f22fd5da1a87a654689
Fred Campo <rickalburo@gmail.com>
2023-01-27 20:57:22 +0800

    added body overflow to restrict scroll

--- budget-calc/src/components/Navbar.svelte
+++ budget-calc/src/components/Navbar.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
   import Button from './Button.svelte'
+
+  export let show = false
 </script>
 
 <nav class="bg-sky-500 shadow-md">
@@ -7,7 +9,13 @@
     class="mx-auto flex flex-wrap items-center justify-between py-4 px-5 sm:max-w-screen-sm sm:px-2"
   >
     <h1 class="text-xl font-semibold tracking-wider">Budget Calculator</h1>
-    <Button class="text-black hover:text-sky-200">
+    <Button
+      class="text-black hover:text-sky-200"
+      on:click={() => {
+        show = true
+        document.body.classList.add('overflow-y-hidden')
+      }}
+    >
       <i class="far fa-plus-square" /> Add Item
     </Button>
   </div>
