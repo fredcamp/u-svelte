7e0d96fdc3ed3ad1380e67274011ef87b81f9a06
Fred Campo <rickalburo@gmail.com>
2023-01-28 21:39:02 +0800

    refactor addexpense and added onclickedit functionalities

--- budget-calc/src/App.svelte
+++ budget-calc/src/App.svelte
@@ -1,23 +1,44 @@
 <script lang="ts">
-  import { setContext, SvelteComponent } from 'svelte'
+  import { setContext, onMount, afterUpdate } from 'svelte'
 
+  import type Expense from './types/Expense'
   import Navbar from './components/Navbar.svelte'
   import Main from './components/Main.svelte'
   import List from './components/List.svelte'
   import Title from './components/Title.svelte'
   import ExpenseForm from './components/ExpenseForm.svelte'
 
-  import expenses from './expenses'
   import { amountParser } from './utils/parser'
+  import { getLocalStorage, setLocalStorage } from './localStorage'
 
-  let data = [...expenses]
+  let data: Expense[] = []
   let showForm = false
-  let addExpenseForm: SvelteComponent
+  let isEditing = false
+
+  let id: string = ''
+  let name: string = ''
+  let amount: number = null
 
   $: total = data.reduce((acc, curr) => acc + curr.amount, 0)
 
   setContext('remove', removeExpense)
   setContext('clear', clearExpenses)
+  setContext('clickedit', onClickEdit)
+
+  onMount(() => (data = getLocalStorage()))
+  afterUpdate(() => setLocalStorage(data))
+
+  // change show value when card update button is clicked
+  function onClickEdit(itemId: string): void {
+    const findItem = data.find((item) => item.id === itemId)
+
+    id = findItem.id
+    name = findItem.name
+    amount = findItem.amount
+
+    showForm = true
+    isEditing = true
+  }
 
   function removeExpense(id: string): void {
     data = data.filter((item) => item.id !== id)
@@ -27,15 +48,15 @@
     data = []
   }
 
-  function addExpense(name: string, amount: number): void {
-    data = [
-      {
-        id: crypto.randomUUID(),
-        name,
-        amount,
-      },
-      ...data,
-    ]
+  function addExpense(): void {
+    const expense = { id: crypto.randomUUID(), name, amount: Number(amount) }
+    data = [expense, ...data]
+  }
+
+  function editExpense(): void {
+    data = data.map((item) => {
+      return item.id === id ? { ...item, name, amount: Number(amount) } : item
+    })
   }
 </script>
 
@@ -58,12 +79,11 @@
   {#if showForm}
     <ExpenseForm
       bind:show={showForm}
-      bind:this={addExpenseForm}
-      on:submit={(e) => {
-        const { name, amount } = e.detail
-        addExpense(name, amount)
-        addExpenseForm.closeForm()
-      }}
+      bind:name
+      bind:amount
+      bind:isEditing
+      on:add={addExpense}
+      on:edit={editExpense}
     />
   {/if}
 </Main>
