bdb1251a913f1419adcd35a36304b9e7b170fc43
Fred Campo <rickalburo@gmail.com>
2023-01-31 02:20:59 +0800

    added main min-height

--- budget-calc/src/components/Main.svelte
+++ budget-calc/src/components/Main.svelte
@@ -1,3 +1,5 @@
-<main class="mx-auto mt-8 px-5 text-sky-900 sm:max-w-screen-sm sm:px-2">
+<main
+  class="mx-auto mt-8 min-h-[80vh] px-5 text-sky-900 sm:max-w-screen-sm sm:px-2"
+>
   <slot />
 </main>
