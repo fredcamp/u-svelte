6c0d34ac0bb986c76e403bb9a84e2aeb3949a014
Fred Campo <rickalburo@gmail.com>
2023-02-01 07:07:00 +0800

    created home hero component

---
+++ razors-ecommerce/src/components/Hero.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+</script>
+
+<header
+  class="bg-hero-background z-40 h-screen bg-cover bg-center bg-no-repeat"
+>
+  <div class="text-primary-light h-full bg-slate-800/50">
+    <div
+      class="flex h-full flex-col items-start justify-center space-y-4 px-6 md:mx-auto md:max-w-screen-md"
+    >
+      <h2
+        class="font-serif text-5xl font-semibold drop-shadow-2xl sm:text-6xl md:text-7xl"
+      >
+        Foam, Shave, & Grow
+      </h2>
+      <p class="text-xl drop-shadow-2xl sm:text-2xl">
+        Embrace your choices &mdash; we do
+      </p>
+      <slot />
+    </div>
+  </div>
+</header>
