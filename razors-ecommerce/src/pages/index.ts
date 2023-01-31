588f1515865d31463c78e624e1c116fecd8f44ab
Fred Campo <rickalburo@gmail.com>
2023-02-01 07:08:46 +0800

    added pages

---
+++ razors-ecommerce/src/pages/index.ts
@@ -0,0 +1,6 @@
+export { default as Home } from './Home.svelte'
+export { default as About } from './About.svelte'
+export { default as Checkout } from './Checkout.svelte'
+export { default as Login } from './Login.svelte'
+export { default as Products } from './Products.svelte'
+export { default as ProductsTemplate } from './ProductsTemplate.svelte'
