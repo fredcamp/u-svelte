f3971f5971f0b5d963e0465d4c30b20022bb4bcf
Fred Campo <rickalburo@gmail.com>
2023-02-01 07:05:13 +0800

    added tw plugins

--- razors-ecommerce/tailwind.config.cjs
+++ razors-ecommerce/tailwind.config.cjs
@@ -1,8 +1,28 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: ['./index.html', './src/**/*.{svelte,ts,js}'],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        xs: '450px',
+        ...defaultTheme.screens,
+      },
+      fontFamily: {
+        sans: ['Poppins', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        primary: {
+          DEFAULT: '#f47e60',
+          light: '#ffcd9a',
+          dark: '#e15b64',
+        },
+      },
+      backgroundImage: {
+        'hero-background': "url('./src/assets/images/hero.jpg')",
+      },
+    },
   },
   plugins: [],
 }
