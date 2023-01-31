67133c4171b28106881c55d229d5a99ae14d0437
Fred Campo <rickalburo@gmail.com>
2023-02-01 07:06:39 +0800

    created button component

---
+++ razors-ecommerce/src/components/Button.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  export let type: 'button' | 'submit' = 'button'
+  let className: string = ''
+
+  export { className as class }
+</script>
+
+<button {type} class={className} {...$$restProps} on:click>
+  <slot />
+</button>
