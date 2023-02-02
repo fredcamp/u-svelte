fef17baf2f41b4775da069a306b51f8c79026884
Fred Campo <rickalburo@gmail.com>
2023-02-02 19:22:11 +0800

    created loading component

---
+++ razors-ecommerce/src/components/Loading.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import loading from '../assets/images/loading.svg'
+
+  export let height: number = 80
+</script>
+
+<div class="relative col-[1/-1] flex items-center justify-center">
+  <p class="absolute font-semibold tracking-widest text-white">Loading...</p>
+  <img src={loading} alt="loading" class="h-{height}" />
+</div>
