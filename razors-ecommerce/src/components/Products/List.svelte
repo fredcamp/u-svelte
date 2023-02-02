35ec01b47b0d64911dbd31462512a6e7119ceadd
Fred Campo <rickalburo@gmail.com>
2023-02-02 19:24:16 +0800

    refactor using loading component

--- razors-ecommerce/src/components/Products/List.svelte
+++ razors-ecommerce/src/components/Products/List.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
   import products from '../../stores/products'
   import Card from './Card.svelte'
-  import loading from '../../assets/images/loading.svg'
+  import Loading from '../Loading.svelte'
 
   export let title: string
 </script>
@@ -9,18 +9,13 @@
 <h2 class="text-center text-2xl font-semibold">{title}</h2>
 
 <div class="mt-10">
-  <ul class="xs:grid-cols-2 grid gap-10 sm:grid-cols-3">
+  <ul class="xs:grid-cols-2 grid gap-10 sm:grid-cols-3 md:grid-cols-4">
     {#each $products as product (product.id)}
       <li>
         <Card {...product} />
       </li>
     {:else}
-      <div class="relative col-[1/-1] flex items-center justify-center">
-        <p class="absolute font-semibold tracking-widest text-white">
-          Loading...
-        </p>
-        <img src={loading} alt="loading" class="h-80" />
-      </div>
+      <Loading />
     {/each}
   </ul>
 </div>
