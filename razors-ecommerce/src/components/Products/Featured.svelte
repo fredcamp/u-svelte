b1d5feef555960bca32a98db2dd852332749be5a
Fred Campo <rickalburo@gmail.com>
2023-02-04 19:18:51 +0800

    create feature component

---
+++ razors-ecommerce/src/components/Products/Featured.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import { derived } from 'svelte/store'
+  import Card from './Card.svelte'
+  import Loading from '../Loading.svelte'
+  import { featuredProducts } from '../../stores/products'
+</script>
+
+<section class="mt-14">
+  <h2 class="text-center text-2xl font-semibold">Featured Products</h2>
+
+  <ul
+    class="xs:w-[80%] mx-auto mt-8 grid w-[90%] gap-8 sm:w-[90%] sm:grid-cols-2 md:w-full md:grid-cols-4 md:gap-4"
+  >
+    {#each $featuredProducts as product}
+      <li>
+        <Card {...product} />
+      </li>
+    {:else}
+      <Loading height={280} />
+    {/each}
+  </ul>
+</section>
