6b95c661424fa4e9f252240479c60b88c168e8a6
Fred Campo <rickalburo@gmail.com>
2023-02-04 19:16:39 +0800

    update img styling

--- razors-ecommerce/src/components/Loading.svelte
+++ razors-ecommerce/src/components/Loading.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
   import loading from '../assets/images/loading.svg'
 
-  export let height: number = 80
+  export let height: number = 320
 </script>
 
 <div class="relative col-[1/-1] flex items-center justify-center">
   <p class="absolute font-semibold tracking-widest text-white">Loading...</p>
-  <img src={loading} alt="loading" class="h-{height}" />
+  <img src={loading} alt="loading" style:height="{height}px" />
 </div>
