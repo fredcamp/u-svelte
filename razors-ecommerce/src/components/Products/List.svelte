2d174cb991939e5b00f18cca8c3ad992e2a555c3
Fred Campo <rickalburo@gmail.com>
2023-02-04 19:19:56 +0800

    add loading func

--- razors-ecommerce/src/components/Products/List.svelte
+++ razors-ecommerce/src/components/Products/List.svelte
@@ -1,21 +1,34 @@
 <script lang="ts">
-  import products from '../../stores/products'
+  import { onMount } from 'svelte'
+  import type { Product } from '../../types/product.type'
+  import productStore from '../../stores/products'
   import Card from './Card.svelte'
   import Loading from '../Loading.svelte'
 
   export let title: string
+
+  let products: Product[] = []
+  let timeoutId: NodeJS.Timeout
+
+  onMount(() => {
+    timeoutId = setTimeout(() => {
+      products = $productStore
+    }, 500)
+
+    return () => clearTimeout(timeoutId)
+  })
 </script>
 
 <h2 class="text-center text-2xl font-semibold">{title}</h2>
 
-<div class="mt-10">
+<section class="mt-10">
   <ul class="xs:grid-cols-2 grid gap-10 sm:grid-cols-3 md:grid-cols-4">
-    {#each $products as product (product.id)}
+    {#each products as product (product.id)}
       <li>
         <Card {...product} />
       </li>
     {:else}
-      <Loading />
+      <Loading height={380} />
     {/each}
   </ul>
-</div>
+</section>
