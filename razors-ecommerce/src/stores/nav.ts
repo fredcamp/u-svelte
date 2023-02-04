78d2392dfc83fe13698560f51abfe5a69e778338
Fred Campo <rickalburo@gmail.com>
2023-02-04 19:20:25 +0800

    refactor nav store

--- razors-ecommerce/src/stores/nav.ts
+++ razors-ecommerce/src/stores/nav.ts
@@ -1,6 +1,8 @@
 import { writable } from 'svelte/store'
 import type { NavStore } from '../types/navstore.type'
 
+let show = createStore(false)
+
 function createStore(initial: boolean): NavStore {
   const { subscribe, set } = writable(initial)
 
@@ -17,4 +19,21 @@ function createStore(initial: boolean): NavStore {
   }
 }
 
-export let show = createStore(false)
+function getProps({ href, isPartiallyCurrent, isCurrent }): {
+  class?: string
+} {
+  const isActive = href === '/' ? isCurrent : isPartiallyCurrent || isCurrent
+
+  if (isActive) {
+    return {
+      class:
+        'relative capitalize px-2 after:absolute after:-bottom-1 after:left-0 after:h-[3px] after:w-full after:bg-primary-dark after:transition-all after:origin-center',
+    }
+  }
+  return {
+    class:
+      'relative capitalize px-2 after:absolute after:-bottom-1 after:left-0 after:h-[3px] after:w-full after:origin-right after:scale-x-0 after:bg-primary-dark after:transition-transform after:hover:origin-left hover:after:scale-x-100 after:duration-300',
+  }
+}
+
+export { show, getProps }
