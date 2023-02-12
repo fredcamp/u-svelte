c8eb2791c698f638e98fb4bc635e9240c811b531
Fred Campo <rickalburo@gmail.com>
2023-02-12 16:45:18 +0800

    added user localstorage

--- razors-ecommerce/src/stores/localStorage.ts
+++ razors-ecommerce/src/stores/localStorage.ts
@@ -1,4 +1,5 @@
 import type { Cart } from '../types/cart.type'
+import type { User } from '../types/user.type'
 
 function getStorageCart(): Cart[] {
   const cart = JSON.parse(localStorage.getItem('cart'))
@@ -9,4 +10,13 @@ function setStorageCart(cart: Cart[]): void {
   localStorage.setItem('cart', JSON.stringify(cart))
 }
 
-export { getStorageCart, setStorageCart }
+function getStorageUser(): User {
+  const user = JSON.parse(localStorage.getItem('user'))
+  return user ?? { username: null, jwt: null }
+}
+
+function setStorageUser(user: User): void {
+  localStorage.setItem('user', JSON.stringify(user))
+}
+
+export { getStorageCart, setStorageCart, getStorageUser, setStorageUser }
