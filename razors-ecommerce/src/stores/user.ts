2a05b4bc20fd9e7663d1a839976b1dbf0d9b5329
Fred Campo <rickalburo@gmail.com>
2023-02-12 16:47:04 +0800

    modify set store to auto update localstorage

--- razors-ecommerce/src/stores/user.ts
+++ razors-ecommerce/src/stores/user.ts
@@ -1,15 +1,24 @@
 import { writable } from 'svelte/store'
 import type { User, UserStore } from '../types/user.type'
+import { getStorageUser, setStorageUser } from './localStorage'
 
-let user = createUserStore({ username: 'fred', jwt: '' })
+let user = createUserStore(getStorageUser())
 
 function createUserStore(initial: User): UserStore {
   const { subscribe, set } = writable(initial)
 
   return {
     subscribe,
-    set,
+    set: (value: User) => {
+      setStorageUser(value)
+      set(value)
+    },
   }
+}
+
+export function logoutUser(): void {
+  user.set({ jwt: null, username: null })
+  localStorage.clear()
 }
 
 export default user
