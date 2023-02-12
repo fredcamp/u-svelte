15bdbd221f5d9f229cf12f6b8b17da28d96ad108
Fred Campo <rickalburo@gmail.com>
2023-02-12 16:48:08 +0800

    added regsier user strapi

---
+++ razors-ecommerce/src/strapi/registerUser.ts
@@ -0,0 +1,27 @@
+import url from './url'
+import type { UserStrapi } from '../types/user.type'
+
+export default async function (
+  username: string,
+  email: string,
+  password: string
+): Promise<UserStrapi> {
+  try {
+    const response = await fetch(`${url}/api/auth/local/register`, {
+      method: 'POST',
+      body: JSON.stringify({ email, username, password }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+
+    if (!response.ok) {
+      throw new Error(response.statusText)
+    }
+
+    const data = await response.json()
+    return data
+  } catch (error) {
+    console.log(error)
+  }
+}
