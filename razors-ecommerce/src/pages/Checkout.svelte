52397b83add2aff605dde26d2f580a6d964025d0
Fred Campo <rickalburo@gmail.com>
2023-02-12 16:43:53 +0800

    added checkout head

--- razors-ecommerce/src/pages/Checkout.svelte
+++ razors-ecommerce/src/pages/Checkout.svelte
@@ -2,6 +2,10 @@
   import Main from '../components/Main.svelte'
 </script>
 
-<Main class="mt-32 min-h-screen px-6 lg:mx-auto lg:max-w-screen-lg">
+<svelte:head>
+  <title>Razors | Checkout</title>
+</svelte:head>
+
+<Main class="mt-32 px-6 lg:mx-auto lg:max-w-screen-lg">
   <h1>Checkout</h1>
 </Main>
