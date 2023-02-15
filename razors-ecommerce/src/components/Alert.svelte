d6dd4f75fd50f499ed074208e8fdfb92011688a3
Fred Campo <rickalburo@gmail.com>
2023-02-15 21:00:03 +0800

    modify snackbar to stop timer when hovered

--- razors-ecommerce/src/components/Alert.svelte
+++ razors-ecommerce/src/components/Alert.svelte
@@ -4,34 +4,68 @@
   import alert from '../stores/alert'
   import Button from './Button.svelte'
 
-  let timeoutId: NodeJS.Timeout
+  let intervalId: NodeJS.Timer
+
+  const duration = 2000
+  const step = 10
+
+  let start = Date.now()
+  let end = start + duration
+  $: progress = ((end - start) / duration) * 100
 
   onMount(() => {
-    timeoutId = setTimeout(() => {
-      onClick()
-    }, 2000)
+    setAlertInterval()
 
     return () => {
-      clearTimeout(timeoutId)
+      clearInterval(intervalId)
     }
   })
+
+  function setAlertInterval() {
+    intervalId = setInterval(() => {
+      start += step
+
+      if (start >= end) {
+        clearInterval(intervalId)
+        onClick()
+      }
+    }, step)
+  }
 
   function onClick() {
     alert.toggleItem('show', false)
   }
+
+  function onHover() {
+    clearInterval(intervalId)
+  }
+
+  function onBlur() {
+    end = end - start + Date.now()
+    start = Date.now()
+    setAlertInterval()
+  }
 </script>
 
 {#if $alert}
+  <!-- svelte-ignore a11y-mouse-events-have-key-events -->
   <div
     class="fixed top-20 left-1/2 z-[999] -translate-x-1/2 text-slate-900"
+    on:mouseover={onHover}
+    on:mouseout={onBlur}
     in:fly={{ y: -200, duration: 400 }}
     out:fade
   >
     <div
-      class="max-w-xs rounded-lg p-8 pb-6 shadow-md {$alert.type === 'success'
+      class="relative max-w-xs overflow-hidden rounded-lg p-8 pb-6 shadow-md {$alert.type ===
+      'success'
         ? 'bg-[#fcaa81]'
         : 'bg-[#ff6a6a]'}"
     >
+      <span
+        class="absolute top-0 left-0 right-0 h-[5px] w-0 bg-slate-900"
+        style="width:{100 - progress}%"
+      />
       <p class="text-sm tracking-wider">{$alert.text}</p>
       <Button
         class="absolute top-2 right-2 px-1 {$alert.type === 'success'
