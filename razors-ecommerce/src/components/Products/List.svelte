145d2c5a03e176237d1678109c18de0334b63d07
Fred Campo <rickalburo@gmail.com>
2023-02-19 01:03:24 +0800

    refactor products

--- razors-ecommerce/src/components/Products/List.svelte
+++ razors-ecommerce/src/components/Products/List.svelte
@@ -1,29 +1,16 @@
 <script lang="ts">
-  import { onMount } from 'svelte'
-  import type { Product } from '../../types/product.type'
   import productStore from '../../stores/products'
   import Card from './Card.svelte'
   import Loading from '../Loading.svelte'
 
   export let title: string
-
-  let products: Product[] = []
-  let timeoutId: NodeJS.Timeout
-
-  onMount(() => {
-    timeoutId = setTimeout(() => {
-      products = $productStore
-    }, 500)
-
-    return () => clearTimeout(timeoutId)
-  })
 </script>
 
 <h2 class="text-center text-2xl font-semibold">{title}</h2>
 
 <section class="mt-10">
   <ul class="xs:grid-cols-2 grid gap-10 sm:grid-cols-3 md:grid-cols-4">
-    {#each products as product (product.id)}
+    {#each $productStore as product (product.id)}
       <li>
         <Card {...product} />
       </li>
